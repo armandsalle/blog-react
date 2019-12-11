93087565fd0f70640962e67073ec13e11fd5e9c1
armand <armand@macbook-pro-darmand.home>
2019-12-11 19:10:57 +0100

    create archi and data

---
+++ src/shared/api/data.js
@@ -0,0 +1,157 @@
+export const posts = [
+  {
+    id: 1,
+    date: "2019-11-15",
+    color: "#D35959",
+    author: "Alain Terrieur",
+    title: "ReactNative vs Flutter",
+    body: `<p>"Back to Dinosaur Island shows how intense, involving and rich virtual reality can be. Players will see a powerful dinosaur breathing inches from their face, getting an unprecedented sense of its immense scale," says David Bowman, Crytek’s director of production. "To watch this scene in 2D is impressive, but when you're in virtual reality, it becomes more intimate, and more exciting -- giving you a feeling of presence and a belief that you’re really right there in a way that no other medium can."</p>
+
+    <p>Pros :</p>
+    <ul>
+        <li>Lorem</li>
+        <li>Ipsum</li>
+    </ul>
+
+    <p>There are <b>also</b> no wires that tangle or yank your phone off your desk or out of your hand. There’s no cord that’s so long that you have to ball it up and stuff it in your pocket, or buy an entirely new accessory just to maintain. Instead, you’re left with two small bits of plastic and metal that slip into (and more importantly, stay in) your ears, objects that conjure music from your phone like some sort of magic trick.</p>`
+  },
+  {
+    id: 2,
+    date: "2019-11-01",
+    color: "#59D3D3",
+    author: "Alex Terrieur",
+    title: "Quel framework JS utiliser en 2020 ?",
+    body: `<p>According to the London Fire Brigade, earlier this month four fire engines and around 20 firefighters were called out to a fire that started in a flat in Southwark and involved an "electric uni-cycle" that was charging. Luckily the owner of the flat heard a loud bang and evacuated the property straight away, so no one was hurt. A similar incident happened a week before in Morden when a charging electric hoverboard caught fire while also charging. Scary stuff.</p>
+    
+    <p>Marley Gray, Microsoft's director of tech strategy for financial services, told Reuters that "the major pain point" it was hearing from customers wanting to use blockchain technology was that it was too hard and too expensive. This, according to Gray, inspired the creation of Ethereum Blockchain as a Service, allowing companies to begin working with the technology without having to first make significant investments in hardware.</p>`
+  },
+  {
+    id: 3,
+    date: "2019-11-01",
+    color: "#D3A959",
+    author: "Yvan Desfrites",
+    title: "Svelte et le SSR",
+    body: `<p>The way Motobot straddles an unmodified Yamaha YZF-R1M bike and blasts down the straight makes for an impressive sight, and its humanoid form means that it can accelerate and adjust its position on the track in an uncannily human manner. Motobot has six actuators for steering, throttle, front brake, rear brake, clutch and gearshift pedal. Reacting to data from the bike's internal sensors, including speed, engine RPM and attitude, it steers the motorcycle and reacts to conditions on the track.</p>
+    
+    <p>Lynx's IP won't increase the resolution of the Structure Sensor, but it cuts back on the sensor mapping errors by up to 90 percent. Occipital says that initial tests reduced the error of its 3D depth maps from 5 inches down to 1/4 inch. "It’s a big deal for the idea of mapping your entire home," says Jeff Powers, Occipital's CEO. "Usually we're limited to an object, or an artifact, or a person. But this tech really lets us blow that out to something like a room."</p>`
+  },
+  {
+    id: 4,
+    date: "#D35959",
+    color: "2019-08-07",
+    author: "Mark Page",
+    title: "TypeScript et le typage",
+    body: `<p>Humanoid robots, say Nasa, will be an integral part of future journeys beyond the Earth. Extreme space environments are often testing and dangerous for human astronauts. Humanoid robots may help make these situations easier to endure by performing mission tasks before humans arrive, or work as "human-assistive robots" alongside a human crew. </p>
+    
+    <p>Machine learning is used to scan emails and understand if they need replying to or not, before creating three response options. An email asking about vacation plans, for example, could be replied to with "No plans yet", "I just sent them to you" or "I'm working on them".</p>`
+  },
+  {
+    id: 5,
+    date: "2019-03-10",
+    color: "#6259D3",
+    author: "Mac First",
+    title: "La SEO et le Google bot",
+    body: `<p>The chosen few will have the chance to fly on one of four US space vessels, including the International Space Station (ISS), and will be selected from a pool of pilots, engineers, scientists and doctors who are willing and able to apply their "considerable knowledge and skills" and "bear tremendous responsibility in orbit". They must also be "determined to succeed", Nasa explained.</p>
+    
+    <p>Without encryption, all of the things we do online would be insecure, be it emailing, or shopping, or banking. They all rely on the principle that if you encrypt data using complex mathematics it is nigh-on impossible to crack. If you're using communication apps such as WhatsApp, Apple's iMessage, WeChat and so on, your messages are encrypted by default.etgwe</p>`
+  },
+  {
+    id: 6,
+    date: "2019-07-26",
+    color: "#D3A959",
+    author: "Sarah Croche",
+    title: "Les polyfills sont ils nécessaires ?",
+    body: `<p>The chosen few will have the chance to fly on one of four US space vessels, including the International Space Station (ISS), and will be selected from a pool of pilots, engineers, scientists and doctors who are willing and able to apply their "considerable knowledge and skills" and "bear tremendous responsibility in orbit". They must also be "determined to succeed", Nasa explained.</p>
+    
+    <p>Without encryption, all of the things we do online would be insecure, be it emailing, or shopping, or banking. They all rely on the principle that if you encrypt data using complex mathematics it is nigh-on impossible to crack. If you're using communication apps such as WhatsApp, Apple's iMessage, WeChat and so on, your messages are encrypted by default.etgwe</p>`
+  },
+  {
+    id: 7,
+    date: "2019-10-19",
+    color: "#6259D3",
+    author: "Kimberley Tartine",
+    title: "Le ShadowDOM est t-il si sombre ?",
+    body: `<p>Their startup joins the growing field of regenerative medicine, which is employing such new technologies as 3-D bioprinting to produce living human tissue and bone. Despite some ethical concerns, several companies even hope to engineer complex human organs, suitable for transplants, within a decade or two.</p>
+
+    <p>The team's discovery came from a meta-analysis of more than 6,000 structural MRI scans, which showed that there was "no significant difference in hippocampal size between men and women". The discovery also counters many popular explanations of the differences between men and women.</p>
+    
+    <p>This study appears in the latest edition of Annals of Internal Medicine. After looking at data from over 15,000 people, researchers estimate that men with pot bellies have twice the mortality risk of people who are just overweight or obese. Women with a similar fat distribution had 1.5 times the risk for death.</p>`
+  },
+  {
+    id: 8,
+    date: "2019-02-02",
+    color: "#D35959",
+    author: "Jean Philipe Ervite-monslip",
+    title: "Le 1er BAC +5 pour apprendre Webpack ouvre",
+    body: `<p>Cell therapy makes up over 60% of the regenerative medicine market, most of which is in the area of stem cell therapy. Adult stem cells can be used in the areas of cardiology; musculo-skeletal, including neurology; optometry; dermatology; cosmology; wound healing; and cancer. Stem cell therapy, when combined with immune and gene therapy, shows even greater potential to cure diseases. This new combination of regenerative cell therapies will open a new age of medicine, forever changing how it is practiced. For example, in the future, knee replacements may no longer be needed with regenerative cell therapy. Regenerative medicine is not only driven by new evidence indicating these therapies are capable of curing diseases, but also by the fact that the world’s population is aging.</p>
+
+    <p>A closer look shows that from 1997 to 2004 there was a steady decline but then there was a plateau until 2009 when the current downward trend began to fall again, as noted by CDC Director Tom Frieden. He points out that 2009 was when tobacco taxes increased and when the FDA began regulating tobacco. Also of note was the 2012 launch of a graphic anti-smoking campaign from the CDC, which is credited with preventing 16,000 tobacco-related deaths. The overlap is no coincidence. Studies have shown these actions, plus other steps such as smoking bans, work.</p>`
+  },
+  {
+    id: 9,
+    date: "2019-08-04",
+    color: "#D3A959",
+    author: "Romeo Frigotouceketapri",
+    title: "Le 1er BAC +5 pour apprendre Webpack ouvre",
+    body: `<p>Cisna reassures his young viewers that his 540 Meals diet didn’t consist solely of boring salads or other healthier options: "I had everything. I had Big Macs, I had the Habanero, I had Quarter Pounders with Cheese, I had ice cream cones, I had sundaes. And what’s really amazing, that people find unbelievable, is probably 95% of every day, I had French fries. I love French fries and that was a great part of it."</p>
+
+    <p>Further, they have to create a scenario in which the most pragmatic individuals — surgeons — buy into the simulation mentally, physically and emotionally. This means that the models have to not only look real, but feel and respond realistically. To do this, the Fractured FX team has incorporated 3D printing, 3D scanning, 3D character and special effects makeup artists. Together they produce "artificial tissues that bleed and pulsate, manmade blood vessels that feel like the real thing when doctors insert a catheter and special gels that feel like brain tissue when an endoscope is guided through them."</p>
+    
+    <p>At first, researchers did not see an obvious relationship between coffee consumption and death rates. Study participants who drank between less than a cup of coffee and three cups a day had 5% to 9% lower risk of dying than those who drank no coffee. Those who drank more than three cups a day did not see any benefit. The finding was murky, like previous studies, some of which suggested a benefit and some did not.</p>`
+  },
+  {
+    id: 10,
+    date: "2019-09-09",
+    color: "#59D3D3",
+    author: "Jennyfer Nitablarepasser",
+    title: "Des objets plus rapides avec JSON.parse",
+    body: `<p>ISPs won't hand over details of the actual pages you visit or contents of your communications without a warrant, but site URLs can make it abundantly clear what they're about: you don't visit Wonga.com if you're financially stable, for example. Similarly, location data can say a lot about you - churchgoing, perhaps, or visiting a particular building at particular times when Alcoholics Anonymous just happens to be meeting.</p>
+
+    <p>Microsoft is also making other changes, and in what seems a miserly move – and definitely nothing to do with abuse – the free storage limit on OneDrive is being slashed from 15GB to 5GB. Sadly that's happening for existing users as well as those new to OneDrive, and the free limit will be decreased from early next year. The 15GB camera roll bonus is also being done away with.</p>`
+  }
+]
+
+export const comment = [
+  {
+    id: 1,
+    post_id: 1,
+    author: "Camille Honnete",
+    content: "Team Flutter !"
+  },
+  {
+    id: 2,
+    post_id: 1,
+    author: "Bart Aba",
+    content: "Faut savoir coder pour coder ?"
+  },
+  {
+    id: 3,
+    post_id: 1,
+    author: "Camille Honnete",
+    content: "Non je crois pas, y a pas vraiment de réponse universelle"
+  },
+  {
+    id: 4,
+    post_id: 2,
+    author: "Barbie Chette",
+    content: "jQuery est toujours dans la partie ?"
+  },
+  {
+    id: 5,
+    post_id: 4,
+    author: "Jean Cive",
+    content: "Manque plus que les pointeurs !"
+  },
+  {
+    id: 6,
+    post_id: 4,
+    author: "Jean Cive",
+    content: "Mince, j'ai envie de jouer à la pétanque maintenant"
+  },
+  {
+    id: 7,
+    post_id: 5,
+    author: "H4KC3R du Dimanche",
+    content: "<script>alert('Ca fonctionne');</script>"
+  }
+]
